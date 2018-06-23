502305e9c7ebb790a6065fe185e6aeb37442be6b
imnocoder <me@imnocoder.com>
2018-06-23 11:16:49 +0800

    Initial setup - Chapter 2

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+	user = { 'username': 'Sam' }
+	
+	return render_template('index.html',title='Home',user=user)
+
+@app.route('/hello')
+def hello():
+	return 
