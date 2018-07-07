088f8e29be2f9f23b3b0e6754265bd623f7ea605
imnocodr <imnocodr@gmail.com>
2018-07-07 18:27:46 +0800

    Chapter 10 - Complete

--- app/email.py
+++ app/email.py
@@ -1,12 +1,13 @@
 from flask_mail import Message
-from app import mail
+from app import app, mail
 from flask import render_template
+from threading import Thread
 
-def send_email(subject, sender, recipients, text_body, html_body):
-    msg = Message(subject, sender=sender, recipients=recipients)
-    msg.body = text_body
-    msg.html = html_body
-    mail.send(msg)
+# def send_email(subject, sender, recipients, text_body, html_body):
+#     msg = Message(subject, sender=sender, recipients=recipients)
+#     msg.body = text_body
+#     msg.html = html_body
+#     mail.send(msg)
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
@@ -17,3 +18,13 @@ def send_password_reset_email(user):
                                          user=user, token=token),
                html_body=render_template('email/reset_password.html',
                                          user=user, token=token))
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    Thread(target=send_async_email, args=(app, msg)).start()
--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
+                    TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, \
+                               Length
 from app.models import User
 
 class LoginForm(FlaskForm):
